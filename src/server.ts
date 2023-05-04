81534e1f3049db216a0b3459f26e372ed6e3a655
sitati-elsis <sitatielsis@gmail.com>
2023-05-04 16:55:19 +0300

    define database schema

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+        console.log(" hello from express")
+        res.status(200)
+        res.json({message: "hello world"})
+    }
+);
+
+export default app
