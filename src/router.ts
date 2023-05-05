75616a1b82f96856580fa55b8acd09070a81a61d
sitati-elsis <sitatielsis@gmail.com>
2023-05-05 13:15:42 +0300

    Set up routes and middleware

---
+++ src/router.ts
@@ -0,0 +1,69 @@
+import {Router} from 'express'
+
+const router = Router()
+
+/**
+ * Products
+ */
+
+router.get('/product', () => {
+
+})
+router.get('/product/:id', () => {
+    
+})
+router.post('/product', () => {
+
+})
+router.put('/product/:id', () => {
+    
+})
+router.delete('/product/:id', () => {
+    
+})
+
+/**
+ * Updates
+ */
+
+router.get('/update', () => {
+
+})
+router.get('/update/:id', () => {
+    
+})
+router.post('/update', () => {
+
+})
+router.put('/update/:id', () => {
+    
+})
+router.delete('/update/:id', () => {
+    
+})
+
+/**
+ * Update Point
+ */
+
+/**
+ * Updates
+ */
+
+router.get('/updatepoint', () => {
+
+})
+router.get('/updatepoint/:id', () => {
+    
+})
+router.post('/updatepoint', () => {
+
+})
+router.put('/updatepoint/:id', () => {
+    
+})
+router.delete('/updatepoint/:id', () => {
+    
+})
+
+export default router
